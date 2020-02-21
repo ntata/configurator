10702a494701cb83dd8a9da679cfafa2ca6b2b42
ntata <nandini.tata.15@gmail.com>
2020-02-21 07:52:43 -0600

    refactor creds

--- configurator/files.py
+++ configurator/files.py
@@ -6,11 +6,11 @@ class File():
     def __init__(self):
         pass
 
-    def check_if_file_exists(self, host, file_metadata, file_path, user, password):
+    def check_if_file_exists(self, host, file_metadata, file_path):
         cmd = "test -e {} && echo file exists || echo file not found".format(file_path)
         try:
             c = SshConnection()
-            output, error = c.run_command(cmd, host, user, password)
+            output, error = c.run_command(cmd, host)
             if output[0] == 'file exists\n':
                 return True
             else:
@@ -19,43 +19,43 @@ class File():
             print(e)
 
 
-    def set_file_permissions(self, host, file_metadata, file_path, user, password):
+    def set_file_permissions(self, host, file_metadata, file_path):
         cmd1 = "sudo chown {}:{} {}".format(file_metadata['user_owner'], file_metadata['group_owner'], file_path)
         cmd2 = "sudo chmod {} {}".format(file_metadata['numeric_mode'], file_path)
         try:
             c = SshConnection()
-            output, error = c.run_command(cmd1+'&&'+cmd2, host, user, password)
+            output, error = c.run_command(cmd1+'&&'+cmd2, host)
             if error == []:
                 print("\n\n*** Set permissions on {} on {}".format(file_path, host))
         except Exception as e:
             print(e)
 
 
-    def create_file(self, host, file_data, file_metadata, user, password):
+    def create_file(self, host, file_data, file_metadata):
         file_path = file_metadata['path']+'/'+file_metadata['name']
         cmd = "echo '{}' | sudo tee {}".format(file_data, file_path)
         try:
-            if self.check_if_file_exists(host, file_metadata, file_path, user, password):
+            if self.check_if_file_exists(host, file_metadata, file_path):
                 raise Exception("File already exists on {}!".format(host))
             c = SshConnection()
-            output, error = c.run_command(cmd, host, user, password)
+            output, error = c.run_command(cmd, host)
             if error != []:
                 raise Exception(error[0]+"on {}".format(host))
             #for o in output:
             #    print(o.strip('\n'))
             print("\n\n*** Created file {} on {}".format(file_path, host))
-            self.set_file_permissions(host, file_metadata, file_path, user, password)
+            self.set_file_permissions(host, file_metadata, file_path)
         except Exception as e:
             print(e)
 
-    def delete_file(self, host, file_metadata, user, password):
+    def delete_file(self, host, file_metadata):
         file_path = file_metadata['path']+'/'+file_metadata['name']
         cmd = "sudo rm {}".format(file_path)
         try:
-            if not self.check_if_file_exists(host, file_metadata, file_path, user, password):
+            if not self.check_if_file_exists(host, file_metadata, file_path):
                 raise Exception("File doesn't exist on {}!".format(host))
             c = SshConnection()
-            output, error = c.run_command(cmd, host, user, password)
+            output, error = c.run_command(cmd, host)
             if error != []:
                 raise Exception(error[0]+"on {}".format(host))
             for o in output:
--- configurator/packages.py
+++ configurator/packages.py
@@ -5,7 +5,7 @@ class Packages():
     def __init__(self):
         pass
 
-    def check_if_valid_package(self, package, host, user, password):
+    def check_if_valid_package(self, package, host):
         """
         method to check if the package name is valid and is available
         in the configured apt repositories
@@ -14,7 +14,7 @@ class Packages():
         """
         cmd = "apt-cache search --names-only '^{}'".format(package)
         c = SshConnection()
-        output, error = c.run_command(cmd, host, user, password)
+        output, error = c.run_command(cmd, host)
         if error == [] and output == []:
             print("\n\n*** Not a valid package. Please check the package name or the apt repositories")
             sys.exit(1)
@@ -22,11 +22,11 @@ class Packages():
             return True
 
 
-    def check_if_package_installed(self, package, host, user, password):
+    def check_if_package_installed(self, package, host):
         cmd = "dpkg -L {} | grep 'is not installed'".format(package)
         try:
             c = SshConnection()
-            output, error = c.run_command(cmd, host, user, password)
+            output, error = c.run_command(cmd, host)
             if error == []:
                print("*** '{}' is already installed on '{}'\n\n".format(package, host))
                return True
@@ -37,19 +37,19 @@ class Packages():
             print(e)
             sys.exit(1)
     
-    def install_package(self, host, packages, user, password):
+    def install_package(self, host, packages):
         for package in packages:
            print("*** Checking to see if '{}' is already installed on '{}'\n\n".format(package, host))
-           if self.check_if_valid_package(package, host, user, password):
+           if self.check_if_valid_package(package, host):
                pass
-           if self.check_if_package_installed(package, host, user, password):
+           if self.check_if_package_installed(package, host):
                continue
            else:
                try:
                    print("\n*** Proceeding to install '{}' on '{}'\n".format(package, host))
                    cmd = "sudo apt-get update && sudo apt-get install {} -y".format(package)
                    c = SshConnection()
-                   output, error = c.run_command(cmd, host, user, password)
+                   output, error = c.run_command(cmd, host)
                    if error != [] and error[-1] == "E: Unable to locate package {}\n".format(package):
                        raise Exception("E: Unable to locate package {}\n".format(package))
                    for o in output:
@@ -59,10 +59,10 @@ class Packages():
                    print(e)
                    sys.exit(1)
     
-    def uninstall_package(self, host, packages, user, password):
+    def uninstall_package(self, host, packages):
         for package in packages:
             print("*** Checking for '{}' package on '{}'\n\n".format(package, host))
-            if not self.check_if_package_installed(package, host, user, password):
+            if not self.check_if_package_installed(package, host):
                 print("Nothing to uninstall; '{}' is not installed on '{}'\n\n".format(package, host))
                 continue
             else:
@@ -70,7 +70,7 @@ class Packages():
                     print("\nUninstalling '{}' on '{}'\n".format(package, host))
                     cmd = "sudo apt-get purge {}".format(package)
                     c = SshConnection()
-                    output, error = c.run_command(cmd, host, user, password)
+                    output, error = c.run_command(cmd, host)
                     for o in output:
                         print(o.strip('\n'))
                     #print("*** Successfully uninstalled '{}' from '{}'\n\n".format(package, host))
@@ -78,17 +78,17 @@ class Packages():
                     print(e)
                     sys.exit(1)
     
-    def upgrade_package(self, host, packages, user, password):
+    def upgrade_package(self, host, packages):
         for package in packages:
            print("*** Checking to see if '{}' is installed on '{}'\n\n".format(package, host))
-           if self.check_if_valid_package(package, host, user, password):
+           if self.check_if_valid_package(package, host):
                pass
-           if self.check_if_package_installed(package, host, user, password):
+           if self.check_if_package_installed(package, host):
                try:
                    print("\n*** Proceeding to upgrade '{}' on '{}'".format(package, host))
                    cmd = "sudo apt-get update && sudo apt-get upgrade {} -y".format(package)
                    c = SshConnection()
-                   output, error = c.run_command(cmd, host, user, password)
+                   output, error = c.run_command(cmd, host)
                    if error != [] and error[-1] == "E: Unable to locate package {}\n".format(package):
                        raise Exception("E: Unable to locate package {}\n".format(package))
                    for o in output:
